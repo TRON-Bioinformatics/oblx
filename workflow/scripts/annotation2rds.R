1190c69fb22981ed5d3413b105ae6f64a49bbf78
Jonas Freimuth <jonas.freimuth@tron-mainz.de>
2026-05-07 20:59:22 +0200

    deps(r-env): Fix conda env
    By including all dependencies (as far as possible) as conda packages
    and upgrading splice2neo to v0.6.14. NOTE however, that due to
    <https://github.com/TRON-bioinformatics/splice2neo/issues/6>,
    the container version still is 0.6.13!

--- workflow/scripts/annotation2rds.R
+++ workflow/scripts/annotation2rds.R
@@ -11,6 +11,20 @@ serialized_transcripts <- snakemake@output[["serialized_transcripts"]]
 serialized_transcript_ranges <- snakemake@output[["serialized_transcript_ranges"]]
 serialized_cds <- snakemake@output[["serialized_cds"]]
 
+# Horrible hack that is necessary because there's currently no functional
+# container image for splice2neo>0.6.13 (the first version using txdbmaker),
+# while at the same time the current renv conda env can't be solved when
+# including GenomicFeatures<1.61.1 (the first version to remove the
+# `makeTxDbFromGFF` function).
+# Once <https://github.com/TRON-bioinformatics/splice2neo/issues/6> is resolved,
+# the container splice2neo should be updated and only txdbmaker should be used.
+makeTxDbFromGFF <- if (length(find.package("txdbmaker", quiet = TRUE)) > 0) {
+  txdbmaker::makeTxDbFromGFF
+} else {
+  # We have to assume the function comes from GenomicFeatures.
+  GenomicFeatures::makeTxDbFromGFF
+}
+
 db <- makeTxDbFromGFF(gtf,
   format = "gtf", dataSource = as.character(basename(gtf))
 )
--- workflow/scripts/tx2gene.R
+++ workflow/scripts/tx2gene.R
@@ -7,6 +7,20 @@ library(magrittr)
 gtf <- snakemake@input[["gtf"]]
 tx2gene <- snakemake@output[["tx2gene"]]
 
+# Horrible hack that is necessary because there's currently no functional
+# container image for splice2neo>0.6.13 (the first version using txdbmaker),
+# while at the same time the current renv conda env can't be solved when
+# including GenomicFeatures<1.61.1 (the first version to remove the
+# `makeTxDbFromGFF` function).
+# Once <https://github.com/TRON-bioinformatics/splice2neo/issues/6> is resolved,
+# the container splice2neo should be updated and only txdbmaker should be used.
+makeTxDbFromGFF <- if (length(find.package("txdbmaker", quiet = TRUE)) > 0) {
+  txdbmaker::makeTxDbFromGFF
+} else {
+  # We have to assume the function comes from GenomicFeatures.
+  GenomicFeatures::makeTxDbFromGFF
+}
+
 txdb <- makeTxDbFromGFF(gtf)
 k <- keys(txdb, keytype = "TXNAME")
 df <- AnnotationDbi::select(txdb, k, "GENEID", "TXNAME")
