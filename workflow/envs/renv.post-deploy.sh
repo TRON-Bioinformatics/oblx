2b7a75edd45cbfb25e8b1d0738dc02f0e1982813
Jonas Freimuth <jonas.freimuth@tron-mainz.de>
2026-05-07 21:00:56 +0200

    chore(r-env): Remove cores from post-deploy script
    Can't be guaranteed that those cores are available. Also the dev
    install now is pretty quick.

--- workflow/envs/renv.post-deploy.sh
+++ workflow/envs/renv.post-deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-Rscript -e "remotes::install_git('https://github.com/TRON-Bioinformatics/splice2neo.git', ref = 'v0.6.12', dependencies=FALSE, Ncpus=8)"
+Rscript -e "remotes::install_git('https://github.com/TRON-Bioinformatics/splice2neo.git', ref = 'v0.6.12', dependencies=FALSE)"
