4e2803a0c906af2763c0779d84ad1097b72e747c
Jonas Freimuth <jonas.freimuth@tron-mainz.de>
2026-05-07 21:00:56 +0200

    chore(r-env): Remove cores from post-deploy script
    Can't be guaranteed that those cores are available. Also the dev
    install now is pretty quick.

--- workflow/envs/renv.post-deploy.sh
+++ workflow/envs/renv.post-deploy.sh
@@ -2,4 +2,4 @@
 # liftOver does not have a conda release, so we install it from its source. This
 # has the advantage of reproducibility when compared to a BiocManager install.
 Rscript -e "install.packages('https://www.bioconductor.org/packages/3.23/workflows/src/contrib/liftOver_1.35.0.tar.gz', repos=NULL, type='source')"
-Rscript -e "remotes::install_git('https://github.com/TRON-Bioinformatics/splice2neo.git', ref = 'v0.6.14', dependencies=FALSE, Ncpus=8)"
+Rscript -e "remotes::install_git('https://github.com/TRON-Bioinformatics/splice2neo.git', ref = 'v0.6.14', dependencies=FALSE)"
