690b1ed7aa9a8d53ac2eb46272348e1e0ea977f6
Jonas Freimuth <jonas.freimuth@tron-mainz.de>
2026-05-08 14:05:32 +0200

    deps(r-env): Update splice2neo
    To be consistent with the apptainer image.

--- workflow/envs/renv.post-deploy.sh
+++ workflow/envs/renv.post-deploy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-Rscript -e "remotes::install_git('https://github.com/TRON-Bioinformatics/splice2neo.git', ref = 'v0.6.12', dependencies=FALSE)"
+Rscript -e "remotes::install_git('https://github.com/TRON-Bioinformatics/splice2neo.git', ref = 'v0.6.13', dependencies=FALSE)"
